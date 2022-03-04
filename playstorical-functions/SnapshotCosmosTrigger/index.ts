40140d366e8de503059a9a522ad8cd59706ec07b
Jon Grant <jongrant95@gmail.com>
2022-03-04 18:29:09 +0000

    add out to snapshot function

--- playstorical-functions/SnapshotCosmosTrigger/index.ts
+++ playstorical-functions/SnapshotCosmosTrigger/index.ts
@@ -2,10 +2,17 @@ import { AzureFunction, Context } from "@azure/functions"
 
 import { Snapshot } from '@playstorical/core/models'
 
-const cosmosDBTrigger: AzureFunction = async function (context: Context, documents: Snapshot[]): Promise<void> {
+const cosmosDBTrigger: AzureFunction = async function (context: Context, documents: Snapshot[]): Promise<Snapshot> {
     if (!!documents && documents.length > 0) {
         const doc = documents[0]
         context.log('Document Id: ', doc.id);
+
+        // GET ADDITIONAL TRACKS
+
+        return {
+            ...doc,
+            test: 'MR LOL'
+        } as any
     }
 }
 
