188be2f0ead98d7782a5ea471c46640bdee1fe77
Jon Grant <jongrant95@gmail.com>
2022-03-10 16:56:04 +0000

    cont.

--- playstorical-functions/SnapshotCosmosTrigger/index.ts
+++ playstorical-functions/SnapshotCosmosTrigger/index.ts
@@ -1,36 +1,40 @@
 import { AzureFunction, Context } from "@azure/functions"
 
 import { Snapshot, SnapshotBase, SnapshotTrack } from '@playstorical/core/models'
-import { getBulkOps, getMusicProvider, getPlaystoricalDbProvider } from '@playstorical/core/modules'
+import { getMusicProvider, getPlaystoricalDbProvider } from '@playstorical/core/modules'
 import { getSnapshotTracks } from '@playstorical/core/helpers'
 
 const cosmosDBTrigger: AzureFunction = async function (context: Context, documents: (Snapshot | SnapshotTrack)[]): Promise<void> {
     if (!!documents && documents.length > 0) {
-        const snapshotDoc = documents[0]
-        context.log('Document Id: ', snapshotDoc.id);
+        context.log(`Start processing ${documents.length} items...`);
+        await Promise.all(documents.map(async (snapshotDoc) => {
+            // const snapshotDoc = documents[0]
+            context.log('Document Id: ', snapshotDoc.id);
 
-        const db = getPlaystoricalDbProvider('cosmosdb')
+            const db = getPlaystoricalDbProvider('cosmosdb')
 
-        if (isNewSnapshot(snapshotDoc)) {
-            // ADD ADDITIONAL SNAPSHOT TRACKS
-            const spotify = getMusicProvider('spotify')
-            spotify.authenticate()
+            if (isNewSnapshot(snapshotDoc)) {
+                // ADD ADDITIONAL SNAPSHOT TRACKS
+                const spotify = getMusicProvider('spotify')
+                await spotify.authenticate()
 
-            const nextReqUrl = snapshotDoc.metadata.initAdditionalTracksReq
-            if (nextReqUrl) {
-                // Spotify only lets you get tracks by playlist ID - there is a chance tracks may not be fully
-                // representing the spotify snapshot. This is currently the best option.
-                const tracks = await spotify.getAdditionalTracks(snapshotDoc.playlistId, { nextReqUrl })
-                const snapshotTracks = getSnapshotTracks(snapshotDoc.snapshotId, tracks)
+                const nextReqUrl = snapshotDoc.metadata.initAdditionalTracksReq
+                if (nextReqUrl) {
+                    // Spotify only lets you get tracks by playlist ID - there is a chance tracks may not be fully
+                    // representing the spotify snapshot. This is currently the best option.
+                    const tracks = await spotify.getAdditionalTracks(snapshotDoc.playlistId, { nextReqUrl })
+                    const snapshotTracks = getSnapshotTracks(snapshotDoc.snapshotId, tracks)
 
-                const ops = getBulkOps(snapshotTracks, 'Upsert', { partitionKey: snapshotDoc.snapshotId })
-                await db.upsert(ops, 'snapshot', { partitionKey: snapshotDoc.snapshotId })
+                    console.log(`Adding ${snapshotTracks.length} additional tracks`)
+
+                    await db.upsert(snapshotTracks, 'snapshot', { partitionKey: 'snapshotId' })
+                }
             }
-        }
 
-        // UPSERT PLAYLIST
+            // UPSERT PLAYLIST
 
-        // INSERT SNAPSHOT HEADER
+            // INSERT SNAPSHOT HEADER
+        }));
     }
 }
 
